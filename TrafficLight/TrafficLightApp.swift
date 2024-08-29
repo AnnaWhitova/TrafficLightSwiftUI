5a62edd25b4947e2f346a97198cd50b3ce163067
AnnaWhitova <159627315+AnnaWhitova@users.noreply.github.com>
2024-08-29 21:44:48 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Анна Белова on 29.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
