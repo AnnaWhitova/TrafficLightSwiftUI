cd83b6a022c7509bd2d975a323b472034de66b04
AnnaWhitova <159627315+AnnaWhitova@users.noreply.github.com>
2024-08-30 13:14:11 +0300

    work with project

---
+++ TrafficLight/TrafficLightCircleView.swift
@@ -0,0 +1,18 @@
+//
+//  TrafficLightCircleView.swift
+//  TrafficLight
+//
+//  Created by Анна Белова on 29.08.2024.
+//
+
+import SwiftUI
+
+struct TrafficLightCircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrafficLightCircleView()
+}
